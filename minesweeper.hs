2ebf2f93eacf0179beb549309cbfb652e261f147
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-06-27 21:34:07 +0200

    Define a functional set representing mines

--- minesweeper.hs
+++ minesweeper.hs
@@ -1,1 +1,14 @@
+type Point = (Int, Int)
+type MineSet = Point -> Bool
+
+union :: MineSet -> MineSet -> MineSet
+empty :: MineSet
+empty = \_ -> False
+add :: MineSet -> Point -> MineSet
+add mineset1 point = \point2 -> (point2 == point) || mineset1 point2
+singleton :: Point -> MineSet
+singleton point = \point2 -> (point2 == point)
+union mined1 mined2 = \point -> mined1 point || mined2 point
+
+
 main = putStrLn "hello, world" 
