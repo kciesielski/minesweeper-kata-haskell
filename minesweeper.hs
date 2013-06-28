2c7b1e538e09c12ad41f7d549e940f14fdd7e127
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-06-28 22:51:15 +0200

    Define generic functional set and some simple types for the game

--- minesweeper.hs
+++ minesweeper.hs
@@ -1,14 +1,26 @@
-type Point = (Int, Int)
-type MineSet = Point -> Bool
+module Game where	
 
-union :: MineSet -> MineSet -> MineSet
-empty :: MineSet
-empty = \_ -> False
-add :: MineSet -> Point -> MineSet
-add mineset1 point = \point2 -> (point2 == point) || mineset1 point2
-singleton :: Point -> MineSet
-singleton point = \point2 -> (point2 == point)
-union mined1 mined2 = \point -> mined1 point || mined2 point
+type Set a = a -> Bool
+newEmpty :: Set a
+newEmpty = \_ -> False
+add :: Eq a => Set a -> a -> Set a
+add s e = \i -> (e == i) || contains s i
+contains :: Set a -> a -> Bool
+contains s e = s e
+singleton :: Eq a => a -> Set a
+singleton a = \e -> (a == e)
+union :: Set a -> Set a -> Set a
+union a b = \e -> a e || b e
 
+data Point = Point {x:: Int, y::Int}
+type MineSet = Set Point
+
+data WorldRectangle = WorldRectangle {
+x1:: Int, y1:: Int, x2:: Int, y2:: Int } deriving (Show)
+
+type TileSet = Point -> Int
+data World = World {bounds:: WorldRectangle, mines:: MineSet, tiles:: TileSet}
+
+withinWorld :: WorldRectangle -> Point -> Bool
 
 main = putStrLn "hello, world" 
