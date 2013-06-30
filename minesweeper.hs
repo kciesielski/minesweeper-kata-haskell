111aa26a6582c9d74ef24734f28b5adf367d8701
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-06-30 22:47:32 +0200

    Some more app code and first test-like code

--- minesweeper.hs
+++ minesweeper.hs
@@ -1,4 +1,4 @@
-module Game where	
+module Main where	
 
 type Set a = a -> Bool
 newEmpty :: Set a
@@ -15,12 +15,34 @@ union a b = \e -> a e || b e
 data Point = Point {x:: Int, y::Int}
 type MineSet = Set Point
 
+
 data WorldRectangle = WorldRectangle {
 x1:: Int, y1:: Int, x2:: Int, y2:: Int } deriving (Show)
 
 type TileSet = Point -> Int
+addTile :: TileSet -> Point -> Int -> TileSet
+emptyTileSet :: TileSet
+emptyTileSet = \_ -> -1
+addTile set point int = \p -> if (x p == x point && y p == y point) then int else set p
+
+
 data World = World {bounds:: WorldRectangle, mines:: MineSet, tiles:: TileSet}
 
 withinWorld :: WorldRectangle -> Point -> Bool
 
-main = putStrLn "hello, world" 
+withinWorld rect = \point -> x1 rect <= x point && x2 rect >= x point && y1 rect <= y point && y2 rect >= y point
+
+-- -------------------- tests ------------
+main :: IO()
+main = do
+	putStrLn("Starting test suite")
+	putStrLn("Testing tileset")
+	putStrLn("It should correctly add a tile to the set")
+	-- given
+	let point = Point {x = 1, y = 1}
+	-- when
+	let gameTiles = addTile emptyTileSet point 12
+	-- then
+	let result = gameTiles point == 12
+	if not result then error "FAIL!" else putStrLn("PASSED")
+	putStrLn("All tests passed succesfully!")
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cabal build
+./dist/build/haskell-minesweeper/haskell-minesweeper
