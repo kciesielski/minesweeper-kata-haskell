9ca403fa81ab5aeb4bc74e536b0acd432d34ba5e
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-07-01 21:49:46 +0200

    Refactor test to make it more given-when-then like

--- minesweeper.hs
+++ minesweeper.hs
@@ -15,7 +15,6 @@ union a b = \e -> a e || b e
 data Point = Point {x:: Int, y::Int}
 type MineSet = Set Point
 
-
 data WorldRectangle = WorldRectangle {
 x1:: Int, y1:: Int, x2:: Int, y2:: Int } deriving (Show)
 
@@ -25,24 +24,29 @@ emptyTileSet :: TileSet
 emptyTileSet = \_ -> -1
 addTile set point int = \p -> if (x p == x point && y p == y point) then int else set p
 
-
 data World = World {bounds:: WorldRectangle, mines:: MineSet, tiles:: TileSet}
 
 withinWorld :: WorldRectangle -> Point -> Bool
-
 withinWorld rect = \point -> x1 rect <= x point && x2 rect >= x point && y1 rect <= y point && y2 rect >= y point
 
--- -------------------- tests ------------
+-- -------------------- test "framework" ;) ----------------------
+assertTrue :: (Monad m) => Bool -> m ()
+assertTrue correct = if not correct then fail "FAIL!" else return()
+assertFalse :: (Monad m) => Bool -> m ()
+assertFalse condition = assertTrue (not condition)
+type TestCase = IO ()
+-- -------------------- tests ------------------------------------
+it_should_correctly_add_a_tile_to_the_set :: TestCase
+it_should_correctly_add_a_tile_to_the_set = do	
+	-- given
+	point <- return (Point {x = 1, y = 1})
+	-- when
+	gameTiles <- return (addTile emptyTileSet point 2)
+	-- then
+	assertTrue (gameTiles point == 2)
+-- -------------- suite execution --------------------------------
 main :: IO()
 main = do
 	putStrLn("Starting test suite")
-	putStrLn("Testing tileset")
-	putStrLn("It should correctly add a tile to the set")
-	-- given
-	let point = Point {x = 1, y = 1}
-	-- when
-	let gameTiles = addTile emptyTileSet point 12
-	-- then
-	let result = gameTiles point == 12
-	if not result then error "FAIL!" else putStrLn("PASSED")
+	it_should_correctly_add_a_tile_to_the_set
 	putStrLn("All tests passed succesfully!")
